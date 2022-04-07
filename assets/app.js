1a5ad1a765d0bc8e65c340af85c89b19743c0da6
Sapheko <quentin.pisli@gmail.com>
2022-04-07 16:34:13 +0200

    Affichage menu ok

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+function burger_menu() {
+    if($('#menu').css('display') == "none") {
+        $('#menu').css('display','block');
+    } else {
+        $('#menu').css('display','none');
+    }    
+}
