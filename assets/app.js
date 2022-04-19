6ff9c0dbeb3524b126b4271986ffbf553a2e4654
Q_Sapheko <quentin.pisli@gmail.com>
2022-04-19 21:58:28 +0200

    UX fix

--- assets/app.js
+++ assets/app.js
@@ -1,7 +1,9 @@
 function burger_menu() {
     if($('#menu').css('display') == "none") {
         $('#menu').slideDown("slow");
+        $('.head').css("background", "black");
     } else {
         $('#menu').slideUp("slow");
+        $('.head').css("background", "transparent");
     }    
 }
